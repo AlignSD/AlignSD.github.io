78c9c0dcc80c8459bc397fb0fed171e00df2376e
Evan Alioto <EvanJAlioto@gmail.com>
2021-04-18 19:16:04 -0700

    structured portfolio secion better, need to change the style of the buttons there, also centered things on the navbar and footer better

--- src/components/pages/PortfolioPage.jsx
+++ src/components/pages/PortfolioPage.jsx
@@ -16,43 +16,43 @@ function PortfolioPage() {
         <div style={styles} className="vertical-center d-flex align-items-center" id="portfolio" >
             <div className="container col-12 portfoliomargin">
                 <main role="main" className="row justify-content-center align-items-stretch portfoliomargin">
-                    <div className="card text-center portfolio-card mr-2 mt-2" style={{ width: "18rem" }}>
+                    <div className="card text-center portfolio-card mr-5 ml-5 mb-5 mt-5 smallScreenPort col-lg-5 col-sm" >
                         <img src={Tonelist} className="card-img-top" alt="screen with code" />
                         <div className="card-body">
                             <h5 className="card-title">Tonelist</h5>
                             <p className="card-text">A music webapp where you can find and listen to similar bands in your saved list</p>
-                            <a href="https://alignsd.github.io/Tonelist/index.html" className="btn btn-primary">Live Product</a>
-                            <a href="https://github.com/AlignSD/Tonelist" className="btn btn-primary">Github Page</a>
+                            <a href="https://alignsd.github.io/Tonelist/index.html" className="portBtn btn btn-primary">Live Product</a>
+                            <a href="https://github.com/AlignSD/Tonelist" className="portBtn btn btn-primary">Github Page</a>
                         </div>
                     </div>
-                    <div className="card text-center portfolio-card mr-2 mt-2" style={{ width: "18rem" }}>
+                    <div className="card text-center portfolio-card mr-5 ml-5 mb-5 mt-5 smallScreenPort col-lg-5 col-sm">
                         <img src={planIt} className="card-img-top" alt="screen with code" />
                         <div className="card-body">
                             <h5 className="card-title">Plan.it</h5>
                             <p className="card-text">A daily planner webapp utilizing API services from moment.js</p>
-                            <a href="https://alignsd.github.io/Plan.it/" className="btn btn-primary">Live Product</a>
-                            <a href="https://github.com/AlignSD/Plan.it" className="btn btn-primary">Github Page</a>
+                            <a href="https://alignsd.github.io/Plan.it/" className="portBtn btn btn-primary margin-right-20">Live Product</a>
+                            <a href="https://github.com/AlignSD/Plan.it" className="portBtn btn btn-primary mx-20">Github Page</a>
                         </div>
                     </div>
                     <div className="w-100"></div>
 
-                    <div className="card text-center portfolio-card mr-2 mt-2" style={{ width: "18rem" }}>
+                    <div className="card text-center portfolio-card mr-5 ml-5 mb-5 mt-5 smallScreenPort col-lg-5 col-sm" >
                         <img src={CastrSS} className="card-img-top" alt="screen with code" />
                         <div className="card-body">
                             <h5 className="card-title">4Castr</h5>
                             <p className="card-text">A live weather and 5 day forecast webapp utilizing API services from openweathermap.org</p>
-                            <a href="https://alignsd.github.io/4Castr" className="btn btn-primary">Live Product</a>
-                            <a href="https://github.com/AlignSD/4Castr" className="btn btn-primary">Github Page</a>
+                            <a href="https://alignsd.github.io/4Castr" className="portBtn btn btn-primary">Live Product</a>
+                            <a href="https://github.com/AlignSD/4Castr" className="portBtn btn btn-primary">Github Page</a>
                         </div>
                     </div>
 
-                    <div className="card text-center portfolio-card mr-2 mt-2" style={{ width: "18rem" }}>
+                    <div className="card text-center portfolio-card mr-5 ml-5 mb-5 mt-5 smallScreenPort col-lg-5 col-sm" >
                         <img src={NuPassScreenShot} className="card-img-top" alt="screen with code" />
                         <div className="card-body">
                             <h5 className="card-title">NuPass</h5>
                             <p className="card-text">A Password Generator</p>
-                            <a href="https://alignsd.github.io/NuPass" className="btn btn-primary">Live Product</a>
-                            <a href="https://github.com/AlignSD/NuPass" className="btn btn-primary">Github Page</a>
+                            <a href="https://alignsd.github.io/NuPass" className="btn btn-primary portBtn ">Live Product</a>
+                            <a href="https://github.com/AlignSD/NuPass" className="portBtn btn btn-primary">Github Page</a>
                         </div>
                     </div>
 

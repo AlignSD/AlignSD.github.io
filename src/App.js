22750fe0e743222adccad957066afcef8cf03834
Evan Alioto <EvanJAlioto@gmail.com>
2021-04-24 04:56:30 -0700

    added particles and typed modules to header section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavTabs from "./components/NavTabs";
 import Footer from "./components/Footer";
 import Home from "./components/pages/HomePage";
 import Portfolio from "./components/pages/PortfolioPage";
+import Particles from 'react-particles-js';
 // import Contact from "./components/pages/ContactPage";
 import "../src/style.css";
 
@@ -15,8 +16,46 @@ function App() {
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
       <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Roboto&display=swap" rel="stylesheet"/>
       <div>
+      <Particles className='particles-canvas'
+      
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            links: {
+              color: "rgb(6, 240, 209, 1)",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            shape: {
+              type: 'square',
+              stroke: {
+                width: 6,
+                color: 'rgb(255, 165, 0)'
+              }
+            }
+          },
+        }}
+        style={{
+          width: '100%',
+          height: '50%',
+        }}
+        
+      />
         <NavTabs />
+        
         <Home />
+        
         <Portfolio />
         {/* <Switch>
         <Route exact path="/" component={Home} />

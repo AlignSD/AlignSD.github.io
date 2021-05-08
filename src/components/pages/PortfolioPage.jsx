16ccffdb4d5a296f10057d6d209f2ed698ce40dd
Evan Alioto <EvanJAlioto@gmail.com>
2021-05-07 23:57:03 -0700

    hover effects implemented on buttons

--- src/components/pages/PortfolioPage.jsx
+++ src/components/pages/PortfolioPage.jsx
@@ -13,6 +13,10 @@ const styles = {
     marginTop: '20'
 };
 
+const buttons = {
+    background: "orange"
+}
+
 function PortfolioPage() {
     return (
         <div style={styles} className="vertical-center d-flex align-items-center safari10 scrollTop scrollTopSmall " id="portfolio" >
@@ -24,8 +28,8 @@ function PortfolioPage() {
                             <h5 className="card-title">Social Media App</h5>
                             <p className="card-text">Simple social media app that allows users to register, post, comment, like/dislike and delete with user
 permissions. </p>
-                            <a href="https://social-media-merng-app.herokuapp.com/" className="portBtn btn btn-primary">Live Product</a>
-                            <a href="https://github.com/AlignSD/SocialMediaApp" className="portBtn btn btn-primary">Github Page</a>
+                            <a href="https://social-media-merng-app.herokuapp.com/" className="portBtn btn btn-primary portfolioButtons" style={styles}>Live Product</a>
+                            <a href="https://github.com/AlignSD/SocialMediaApp" className="portBtn btn btn-primary portfolioButtons">Github Page</a>
                         </div>
                     </div>
                     <div className="card text-center portfolio-card mr-5 ml-5 mb-5  smallScreenPort col-lg-5 col-sm">
@@ -33,8 +37,8 @@ permissions. </p>
                         <div className="card-body">
                             <h5 className="card-title">Cryptick</h5>
                             <p className="card-text">Blockchain Ticketing Service utilizing the Ethereum blockchain thru deployed smart contracts.</p>
-                            <a href="https://alignsd.github.io/Plan.it/" className="portBtn btn btn-primary margin-right-20">Live Product</a>
-                            <a href="https://github.com/AlignSD/Ticket_Bits" className="portBtn btn btn-primary mx-20">Github Page</a>
+                            <a href="https://alignsd.github.io/Plan.it/" className="portBtn btn btn-primary margin-right-20 portfolioButtons">Live Product</a>
+                            <a href="https://github.com/AlignSD/Ticket_Bits" className="portBtn btn btn-primary mx-20 portfolioButtons">Github Page</a>
                         </div>
                     </div>
                     <div className="w-100"></div>
@@ -44,8 +48,8 @@ permissions. </p>
                         <div className="card-body">
                             <h5 className="card-title">Tonelist</h5>
                             <p className="card-text">A music webapp where you can find and listen to similar bands in your saved list</p>
-                            <a href="https://alignsd.github.io/Tonelist/index.html" className="portBtn btn btn-primary">Live Product</a>
-                            <a href="https://github.com/AlignSD/Tonelist" className="portBtn btn btn-primary">Github Page</a>
+                            <a href="https://alignsd.github.io/Tonelist/index.html" className="portBtn btn btn-primary portfolioButtons">Live Product</a>
+                            <a href="https://github.com/AlignSD/Tonelist" className="portBtn btn btn-primary portfolioButtons">Github Page</a>
                         </div>
                     </div>
                     <div className="card text-center portfolio-card mr-5 ml-5 mb-5  smallScreenPort col-lg-5 col-sm">
@@ -53,8 +57,8 @@ permissions. </p>
                         <div className="card-body">
                             <h5 className="card-title">Plan.it</h5>
                             <p className="card-text">A daily planner webapp utilizing API services from moment.js</p>
-                            <a href="https://alignsd.github.io/Plan.it/" className="portBtn btn btn-primary margin-right-20">Live Product</a>
-                            <a href="https://github.com/AlignSD/Plan.it" className="portBtn btn btn-primary mx-20">Github Page</a>
+                            <a href="https://alignsd.github.io/Plan.it/" className="portBtn btn btn-primary margin-right-20 portfolioButtons">Live Product</a>
+                            <a href="https://github.com/AlignSD/Plan.it" className="portBtn btn btn-primary mx-20 portfolioButtons">Github Page</a>
                         </div>
                     </div>
                     <div className="w-100"></div>
@@ -64,8 +68,8 @@ permissions. </p>
                         <div className="card-body">
                             <h5 className="card-title">4Castr</h5>
                             <p className="card-text">A live weather and 5 day forecast webapp utilizing API services from openweathermap.org</p>
-                            <a href="https://alignsd.github.io/4Castr" className="portBtn btn btn-primary">Live Product</a>
-                            <a href="https://github.com/AlignSD/4Castr" className="portBtn btn btn-primary">Github Page</a>
+                            <a href="https://alignsd.github.io/4Castr" className="portBtn btn btn-primary portfolioButtons">Live Product</a>
+                            <a href="https://github.com/AlignSD/4Castr" className="portBtn btn btn-primary portfolioButtons">Github Page</a>
                         </div>
                     </div>
 
@@ -74,8 +78,8 @@ permissions. </p>
                         <div className="card-body">
                             <h5 className="card-title">NuPass</h5>
                             <p className="card-text">A Password Generator</p>
-                            <a href="https://alignsd.github.io/NuPass" className="btn btn-primary portBtn ">Live Product</a>
-                            <a href="https://github.com/AlignSD/NuPass" className="portBtn btn btn-primary">Github Page</a>
+                            <a href="https://alignsd.github.io/NuPass" className="btn btn-primary portBtn portfolioButtons">Live Product</a>
+                            <a href="https://github.com/AlignSD/NuPass" className="portBtn btn btn-primary portfolioButtons">Github Page</a>
                         </div>
                     </div>
 

b724df087262a9b80a39c8b55b1dc42b1442a50c
Evan Alioto <EvanJAlioto@gmail.com>
2021-08-10 23:39:22 -0700

    added justify content center class to footer contact section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,7 +31,7 @@ function Footer() {
       <div className="container contact">
         <div className="row">
           {/*content for phone */}
-          <div className="col-md-4 col-lg-4 contact-box pt-1 d-md-block d-lg-flex d-flex">
+          <div className="col-md-4 col-lg-4 contact-box pt-1 d-md-block d-lg-flex d-flex justify-content-lg-center">
             <div className="contact-box__icon"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-phone-call" viewBox="0 0 24 24" stroke-width="1" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
   <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
